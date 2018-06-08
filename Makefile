1c0f5da87af5eb1d9e0ccf0e044e906d6048828b
Tyr Chen <tyr.chen@gmail.com>
2018-06-09 05:16:26 +0800

    enable travis and makefile

---
+++ Makefile
@@ -0,0 +1,61 @@
+TOP_DIR=.
+README=$(TOP_DIR)/README.md
+
+VERSION=$(strip $(shell cat version))
+
+build:
+	@echo "Building the software..."
+	@mix compile
+
+init: install dep
+	@echo "Initializing the repo..."
+	@git submodule update --init --recursive
+
+travis-init:
+	@echo "Initialize software required for travis (normally ubuntu software)"
+
+install:
+	@echo "Install software required for this repo..."
+	@mix local.hex --force
+	@mix local.rebar --force
+
+dep:
+	@echo "Install dependencies required for this repo..."
+	@mix deps.get
+
+pre-build: install dep
+	@echo "Running scripts before the build..."
+
+post-build:
+	@echo "Running scripts after the build is done..."
+
+all: pre-build build post-build all-api-docs
+
+test:
+	@echo "Running test suites..."
+
+lint:
+	@echo "Linting the software..."
+
+doc:
+	@echo "Building the documenation..."
+
+precommit: dep build
+	@mix test_all
+
+travis: precommit
+
+travis-deploy: release
+	@echo "Deploy the software by travis"
+
+clean:
+	@echo "Cleaning the build..."
+	@rm -rf _build
+
+run:
+	@echo "Running the software..."
+	@iex -S mix
+
+include .makefiles/*.mk
+
+.PHONY: build init travis-init install dep pre-build post-build all test doc precommit travis clean watch run bump-version create-pr
--- mix.exs
+++ mix.exs
@@ -9,8 +9,21 @@ defmodule Tub.MixProject do
       app: :tub,
       version: @version,
       elixir: @elixir_version,
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+
+      # exdocs
+      # Docs
+      name: "Tub",
+      source_url: "https://github.com/tyrchen/tub",
+      homepage_url: "https://github.com/tyrchen/tub",
+      docs: [
+        main: "Tub",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -21,7 +34,9 @@ defmodule Tub.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   defp deps do
     [
       {:absinthe, "~> 1.4.0", only: [:dev, :test]},
@@ -30,6 +45,24 @@ defmodule Tub.MixProject do
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.18", only: [:dev, :test]},
       {:pre_commit_hook, "~> 1.2", only: [:dev]}
+    ]
+  end
+
+  defp description do
+    """
+    Load a single beam file, an app (a set of beams), or an erlang release (a set of apps) to a node.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*", "version", ".elixir_version"],
+      licenses: ["MIT"],
+      maintainers: ["tyr.chen@gmail.com"],
+      links: %{
+        "GitHub" => "https://github.com/tyrchen/tub",
+        "Docs" => "https://hexdocs.pm/tub"
+      }
     ]
   end
 end
