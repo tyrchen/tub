5833a76eef80af2a774c370d8e772253406779d1
Tyr Chen <tyr.chen@gmail.com>
2018-06-26 17:34:54 -0700

    allow metadata in create table (#7)
    * allow metadata in create table
    
    * bump version

--- test/gen/sql/sql_test.exs
+++ test/gen/sql/sql_test.exs
@@ -4,9 +4,14 @@ defmodule Tub.Sql.CreateTableTest do
 
   test "generate a create table sql" do
     name = "Block"
-    fields = [height: :integer, hash: :string, gas: :integer]
 
-    assert "create table Block (\n  height integer,\n  hash string,\n  gas integer\n);" ==
+    fields = [
+      {:height, :integer, null: true},
+      {:hash, :string, null: false},
+      {:gas, :integer, null: true}
+    ]
+
+    assert "create table Block (\n  height integer,\n  hash string not null,\n  gas integer\n);" ==
              CreateTable.gen(name, fields)
   end
 end
